eb95db118c9f8667f171f7208af5484ffd17c425
BeratCeylan07 <beratceylantgthb@gmail.com>
2024-03-07 17:17:48 +0300

    backup
    backup

---
+++ logistik/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace logistik.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["ActivePage"] = "Home";
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        ViewData["ActivePage"] = "About";
+        return View();
+    }
+
+    public IActionResult Services()
+    {
+        ViewData["ActivePage"] = "Services";
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        ViewData["ActivePage"] = "Contact";
+        return View();
+    }
+}
